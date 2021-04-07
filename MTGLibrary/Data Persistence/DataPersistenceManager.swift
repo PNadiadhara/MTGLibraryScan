5401621efea93fb54943cb327b0008e875d79579
PN <priteshrna@gmail.com>
2021-04-07 16:03:22 -0400

    future addition reference code

--- MTGLibrary/Data Persistence/DataPersistenceManager.swift
+++ MTGLibrary/Data Persistence/DataPersistenceManager.swift
@@ -18,4 +18,18 @@ final class DataPersistenceManager {
     static func filepathToDocumentsDirectory(filename: String) -> URL {
         return documentsDirectory().appendingPathComponent(filename)
     }
+    
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    // Reference for saving img as png to reduce api calls
+    
+//    if let image = UIImage(named: "example.png") {
+//        if let data = image.pngData() {
+//            let filename = getDocumentsDirectory().appendingPathComponent("copy.png")
+//            try? data.write(to: filename)
+//        }
+//    }
 }
